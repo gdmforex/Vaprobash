4043a3509293ab76deca347451b2b53203607e7f
brud <63890jkl123>
2015-01-20 14:59:11 +0200

    delete folders + edit .gitignore

--- scripts/memcached.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Install Memcached
-# -qq implies -y --force-yes
-sudo apt-get install -qq memcached
--- test.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-for file in $(find ./scripts -name '*.sh'); do
-    bash -n $file
-done
-
